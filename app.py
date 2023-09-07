7a31142de5f9d6191f0ebabb3add0b560a2fcf5e
Willian Pacheco Silva <willianpacheco31@gmail.com>
2023-09-07 11:58:32 -0300

    refactor: improving the code

--- app.py
+++ app.py
@@ -3,13 +3,11 @@ from cryptography.fernet import Fernet
 import pandas as pd
 import io
 from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from cryptography.hazmat.backends import default_backend
-import os
 import base64
 
 def generate_fernet_key_from_text(text):
-    text_bytes = text.encode('utf-8')  # Encode the string as bytes using UTF-8
+    text_bytes = text.encode('utf-8')
     digest = hashes.Hash(hashes.SHA256(), backend=default_backend())
     digest.update(text_bytes)
     return base64.urlsafe_b64encode(digest.finalize())
@@ -25,13 +23,11 @@ with open("./encrypted_files/investimentos.txt", "rb") as encrypted_file:
     loaded_encrypted_data = encrypted_file.read()
 
 key = st.secrets["encryption_credentials"]["crypto_key"]
-
 key = generate_fernet_key_from_text(key)
 
 decrypted_data = decrypt_data(loaded_encrypted_data, key)
 
 decrypted_df = pd.read_csv(io.StringIO(decrypted_data))
 
-# Mostra os dados descriptografados
 st.text("Dados Descriptografados:")
 st.table(decrypted_df)
