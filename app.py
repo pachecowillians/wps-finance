95a4e944bc20be10ab886446f1c950ada61bab68
Willian Pacheco Silva <willianpacheco31@gmail.com>
2023-09-07 12:05:39 -0300

    refactor: using fernet key

--- app.py
+++ app.py
@@ -2,15 +2,6 @@ import streamlit as st
 from cryptography.fernet import Fernet
 import pandas as pd
 import io
-from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.backends import default_backend
-import base64
-
-def generate_fernet_key_from_text(text):
-    text_bytes = text.encode('utf-8')
-    digest = hashes.Hash(hashes.SHA256(), backend=default_backend())
-    digest.update(text_bytes)
-    return base64.urlsafe_b64encode(digest.finalize())
 
 def decrypt_data(encrypted_data, key):
     f = Fernet(key)
@@ -23,7 +14,6 @@ with open("./encrypted_files/investimentos.txt", "rb") as encrypted_file:
     loaded_encrypted_data = encrypted_file.read()
 
 key = st.secrets["encryption_credentials"]["crypto_key"]
-key = generate_fernet_key_from_text(key)
 
 decrypted_data = decrypt_data(loaded_encrypted_data, key)
 
--- encrypt.py
+++ encrypt.py
@@ -1,15 +1,6 @@
 from cryptography.fernet import Fernet
 import pandas as pd
-from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.backends import default_backend
-import base64
 import streamlit as st
-
-def generate_fernet_key_from_text(text):
-    text_bytes = text.encode('utf-8')
-    digest = hashes.Hash(hashes.SHA256(), backend=default_backend())
-    digest.update(text_bytes)
-    return base64.urlsafe_b64encode(digest.finalize())
 
 def encrypt_data(data, key):
     f = Fernet(key)
@@ -20,11 +11,8 @@ file_path = "./data/investimentos.csv"
 df = pd.read_csv(file_path)
 
 key = st.secrets["encryption_credentials"]["crypto_key"]
-key = generate_fernet_key_from_text(key)
 
 encrypted_data = encrypt_data(df.to_csv(index=False), key)
 
 with open("./encrypted_files/investimentos.txt", "wb") as encrypted_file:
     encrypted_file.write(encrypted_data)
-
-print(Fernet.generate_key())
