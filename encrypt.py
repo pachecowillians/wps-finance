f1844478aad911677e83d50b822cfc2911a2bfcb
Willian Pacheco Silva <willianpacheco31@gmail.com>
2023-09-07 11:46:29 -0300

    refactor: removing unused libraries

--- encrypt.py
+++ encrypt.py
@@ -1,14 +1,11 @@
 from cryptography.fernet import Fernet
 import pandas as pd
 from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from cryptography.hazmat.backends import default_backend
-import os
 import base64
-import hashlib
 
 def generate_fernet_key_from_text(text):
-    text_bytes = text.encode('utf-8')  # Encode the string as bytes using UTF-8
+    text_bytes = text.encode('utf-8')
     digest = hashes.Hash(hashes.SHA256(), backend=default_backend())
     digest.update(text_bytes)
     return base64.urlsafe_b64encode(digest.finalize())
