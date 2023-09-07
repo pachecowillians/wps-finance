5626b7090f6fa936802984b728a335d0ea4cc259
Willian Pacheco Silva <willianpacheco31@gmail.com>
2023-09-07 08:09:10 -0300

    feat: encrypting and decrypting csv file

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from cryptography.fernet import Fernet
 import pandas as pd
+import io
 
 def generate_key():
     return Fernet.generate_key()
@@ -29,13 +30,22 @@ st.text(key)
 
 encrypted_data = encrypt_data(df.to_csv(index=False), key)
 
+with open("./encrypted_files/encrypted_data.txt", "wb") as encrypted_file:
+    encrypted_file.write(encrypted_data)
+
 # Mostra os dados criptografados
 st.text("Dados Criptografados:")
 st.text(encrypted_data)
 
+with open("./encrypted_files/encrypted_data.txt", "rb") as encrypted_file:
+    loaded_encrypted_data = encrypted_file.read()
+
 # Descriptografa os dados
-decrypted_data = decrypt_data(encrypted_data, key)
+decrypted_data = decrypt_data(loaded_encrypted_data, key)
+
+decrypted_df = pd.read_csv(io.StringIO(decrypted_data))
 
 # Mostra os dados descriptografados
 st.text("Dados Descriptografados:")
 st.text(decrypted_data)
+st.table(decrypted_df)
