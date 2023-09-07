48f85666051b78e691b9ead980d6c23ce4fbf114
Willian Pacheco Silva <willianpacheco31@gmail.com>
2023-09-07 11:35:05 -0300

    refactor: splitting code into encrypt and decrypt

--- app.py
+++ app.py
@@ -2,14 +2,17 @@ import streamlit as st
 from cryptography.fernet import Fernet
 import pandas as pd
 import io
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.backends import default_backend
+import os
+import base64
 
-def generate_key():
-    return Fernet.generate_key()
-
-def encrypt_data(data, key):
-    f = Fernet(key)
-    encrypted_data = f.encrypt(data.encode())
-    return encrypted_data
+def generate_fernet_key_from_text(text):
+    text_bytes = text.encode('utf-8')  # Encode the string as bytes using UTF-8
+    digest = hashes.Hash(hashes.SHA256(), backend=default_backend())
+    digest.update(text_bytes)
+    return base64.urlsafe_b64encode(digest.finalize())
 
 def decrypt_data(encrypted_data, key):
     f = Fernet(key)
@@ -18,34 +21,17 @@ def decrypt_data(encrypted_data, key):
 
 st.title("WPS - Finance")
 
-file_path = "./data/investimentos.csv"  # Substitua com o nome do seu arquivo CSV
-df = pd.read_csv(file_path)
-
-# Mostre o DataFrame lido
-st.write(df)
-
-key = generate_key()
-
-st.text(key)
-
-encrypted_data = encrypt_data(df.to_csv(index=False), key)
-
-with open("./encrypted_files/encrypted_data.txt", "wb") as encrypted_file:
-    encrypted_file.write(encrypted_data)
-
-# Mostra os dados criptografados
-st.text("Dados Criptografados:")
-st.text(encrypted_data)
-
-with open("./encrypted_files/encrypted_data.txt", "rb") as encrypted_file:
+with open("./encrypted_files/investimentos.txt", "rb") as encrypted_file:
     loaded_encrypted_data = encrypted_file.read()
 
-# Descriptografa os dados
+key = "teste"
+
+key = generate_fernet_key_from_text(key)
+
 decrypted_data = decrypt_data(loaded_encrypted_data, key)
 
 decrypted_df = pd.read_csv(io.StringIO(decrypted_data))
 
 # Mostra os dados descriptografados
 st.text("Dados Descriptografados:")
-st.text(decrypted_data)
 st.table(decrypted_df)
--- encrypt.py
+++ encrypt.py
@@ -1,36 +1,22 @@
 from cryptography.fernet import Fernet
 import pandas as pd
-import io
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from cryptography.hazmat.backends import default_backend
 import os
 import base64
+import hashlib
 
 def generate_fernet_key_from_text(text):
-    backend = default_backend()
-    salt = os.urandom(16)
-
-    kdf = PBKDF2HMAC(
-        algorithm=hashes.SHA256(),
-        length=32,
-        salt=salt,
-        iterations=100000,
-        backend=backend
-    )
-
-    key = base64.urlsafe_b64encode(kdf.derive(text.encode()))
-    return key
+    text_bytes = text.encode('utf-8')  # Encode the string as bytes using UTF-8
+    digest = hashes.Hash(hashes.SHA256(), backend=default_backend())
+    digest.update(text_bytes)
+    return base64.urlsafe_b64encode(digest.finalize())
 
 def encrypt_data(data, key):
     f = Fernet(key)
     encrypted_data = f.encrypt(data.encode())
     return encrypted_data
-
-def decrypt_data(encrypted_data, key):
-    f = Fernet(key)
-    decrypted_data = f.decrypt(encrypted_data).decode()
-    return decrypted_data
 
 file_path = "./data/investimentos.csv"
 df = pd.read_csv(file_path)
@@ -43,12 +29,3 @@ encrypted_data = encrypt_data(df.to_csv(index=False), key)
 
 with open("./encrypted_files/investimentos.txt", "wb") as encrypted_file:
     encrypted_file.write(encrypted_data)
-
-with open("./encrypted_files/investimentos.txt", "rb") as encrypted_file:
-    loaded_encrypted_data = encrypted_file.read()
-
-decrypted_data = decrypt_data(loaded_encrypted_data, key)
-
-decrypted_df = pd.read_csv(io.StringIO(decrypted_data))
-
-print(decrypted_df)
